3ddc2f822bc040e82caca5e36b55771e52eff05f
Igor <62031924+SpaceHolderdJs@users.noreply.github.com>
2020-11-03 00:53:49 +0200

    Added Back module
    Autorization process, User model, 1 routes.

---
+++ app.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const app = express();
+
+//DB
+
+const mongoose = require("mongoose");
+const mongoUrl = `mongodb+srv://admin:taras-igor@cluster0.jffyq.mongodb.net/app?retryWrites=true&w=majority`;
+
+//Autorization module
+
+app.use("/api/auth", require("./routes/auth.routes"));
+
+async function start(){
+
+  try{
+    await mongoose.connect(mongoUrl,{
+      useNewUrlParser:true,
+      useUnifiedTopology:true,
+      useCreateIndex:true
+    });
+
+    app.listen(PORT = 5000,()=>{
+      console.log(`App has been stared on ${PORT}. DB is avalieble`); 
+    });
+
+  }catch(e){
+
+    console.log(`Server error ${e.message}`);
+    process.exit(1);
+
+  }
+}
+
+start();
---
+++ routes/auth.routes.js
@@ -0,0 +1,37 @@
+//This is router for autorization!
+
+const {Router} = require("express");
+const router = Router();
+
+const User = require("../models/User");
+const bcrypt = require("bcryptjs");
+
+router.post("/register", async(req, res)=>{
+    try {
+      const {email, password} = req.body;
+
+      const candidate = await User.findOne({ email });
+
+      if(candidate){
+       return  res.status(400).json({message:"User is already exsists"});
+      }
+
+      const hashedPassword = await bcrypt.hash(password, 12);
+      const user = new User({email, password: hashedPassword});
+
+      await user.save();
+
+      res.status(201).json({message:"User created"});
+
+
+    } catch (e) {
+       res.status(500).json({message:"Something is wrong. Try again"});
+
+    }
+});
+
+router.post("/login", async(req, res)=>{
+  
+});
+
+module.exports = router;
