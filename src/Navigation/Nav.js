91ca2b395fcd444aa117512be149bbeda2854c4f
Taras <lytvynskyi.t@gmail.com>
2020-11-02 18:42:13 +0200

    Сделал навигацию черной

--- src/Navigation/Nav.js
+++ src/Navigation/Nav.js
@@ -3,32 +3,45 @@ import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 function Nav() {
   return (
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-      <Link to="/" class="navbar-brand" href="#">
-        Home
-      </Link>
-      <button
-        class="navbar-toggler"
-        type="button"
-        data-toggle="collapse"
-        data-target="#navbarNavAltMarkup"
-        aria-controls="navbarNavAltMarkup"
-        aria-expanded="false"
-        aria-label="Toggle navigation"
-      >
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-        <div class="navbar-nav">
-          <Link to="/login" class="nav-item nav-link active" href="#">
-            Login <span class="sr-only">(current)</span>
-          </Link>
-          <Link to="/register" class="nav-item nav-link" href="#">
-            Register
-          </Link>
+    <div>
+      <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <Link to="/" class="navbar-brand" href="#">
+          Home
+        </Link>
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarNavAltMarkup"
+          aria-controls="navbarNavAltMarkup"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+          <div class="navbar-nav">
+            <Link to="/login" class="nav-item nav-link active" href="#">
+              Login <span class="sr-only">(current)</span>
+            </Link>
+            <Link to="/register" class="nav-item nav-link" href="#">
+              Register
+            </Link>
+          </div>
         </div>
-      </div>
-    </nav>
+        <form class="form-inline">
+          <input
+            class="form-control mr-sm-2"
+            type="search"
+            placeholder="Search"
+            aria-label="Search"
+          ></input>
+          <button class="btn btn-outline-success my-2 my-sm-0" type="submit">
+            Search
+          </button>
+        </form>
+      </nav>
+    </div>
   );
 }
 export default Nav;
