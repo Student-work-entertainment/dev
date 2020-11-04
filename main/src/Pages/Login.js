88e5ede26def0d7b451a6fe03106540b6fc03b42
Taras <lytvynskyi.t@gmail.com>
2020-11-05 00:51:20 +0200

    Почти добавил логин в бэк

--- main/src/Pages/Login.js
+++ main/src/Pages/Login.js
@@ -1,4 +1,5 @@
-import React from "react";
+import React, {useState} from "react";
+import {useHttp} from "../Hooks/http.hook";
 
 const divContainer = {
   padding: "15px",
@@ -11,20 +12,38 @@ const login = {
 };
 
 function Login() {
+  const [form, setForm] = useState({
+    email: "",
+    password: "",
+  });
+  const {loading,error,request} = useHttp()
+
+  const changeHandler = (event) => {
+    setForm({ ...form, [event.target.name]: event.target.value });
+  };
+  const loginHandelr = async () => {
+    try {
+      const data = await request('/api/auth/login', 'POST', {...form})
+    }catch (e){
+
+    }
+  }
+
   return (
     <div className="container mt-4" style={divContainer}>
       <div>
         <h1 style={login}>Login</h1>
       </div>
-      <form>
         <div className="form-group">
           <label>Email address</label>
           <input
             type="email"
             className="form-control"
-            id="exampleInputEmail1"
+            id="email"
+            name = "email"
             aria-describedby="emailHelp"
             placeholder="Enter email"
+            onChange={changeHandler}
           ></input>
           <small id="emailHelp" className="form-text text-muted">
             We'll never share your email with anyone else.
@@ -35,15 +54,16 @@ function Login() {
           <input
             type="password"
             className="form-control"
-            id="exampleInputPassword1"
+            id="password"
+            name="password"
+            onChange={changeHandler}
             placeholder="Password"
           ></input>
         </div>
         <div className="form-check"></div>
-        <button type="submit" className="btn btn-primary">
+        <button onClick={loginHandelr} type="submit" className="btn btn-primary">
           Login
         </button>
-      </form>
     </div>
   );
 }
--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,86 +1,83 @@
 //This is router for autorization!
 
-const {Router} = require("express");
+const { Router } = require("express");
 const router = Router();
 
 const User = require("../models/User");
 const bcrypt = require("bcryptjs");
 
-const jwt = require('jsonwebtoken')
+const jwt = require("jsonwebtoken");
 
-router.post("/register", async(req, res)=>{
-    try {
-      const errors = validaionResult(req)
+// api/auth/register
+router.post("/register", async (req, res) => {
+  try {
+    //Need fixed
+    // const errors = validaionResult(req)
+    //
+    // if(!errors.isEmpty()){
+    //     return res.status(400).json({
+    //         errors:errors.array(),
+    //         message:'Ivalid data register'
+    //     })
+    // }
 
-      if(!errors.isEmpty()){
-          return res.status(400).json({
-              errors:errors.array(),
-              message:'Ivalid data register'
-          })
-      }
+    const { email, password, FirsName, LastName, secondPassword } = req.body;
 
-      const {email, password, FirsName, LastName, secondPassword} = req.body;
+    const candidate = await User.findOne({ email });
 
-      const candidate = await User.findOne({ email });
-
-      if(candidate){
-       return res.status(400).json({message:"User is already exsists"});
-      }
-
-      const hashedPassword = await bcrypt.hash(password, 12);
-      const user = new User({email, password: hashedPassword,FirsName,LastName,secondPassword});
-
-      await user.save();
-
-      res.status(201).json({message:"User created"});
-
-
-    } catch (e) {
-       res.status(500).json({message:"Something is wrong. Try again"});
+    if (candidate) {
+      return res.status(400).json({ message: "User is already exsists" });
     }
+
+    const hashedPassword = await bcrypt.hash(password, 12);
+    const user = new User({
+      email,
+      password: hashedPassword,
+      FirsName,
+      LastName,
+      secondPassword,
+    });
+
+    await user.save();
+
+    res.status(201).json({ message: "User created" });
+  } catch (e) {
+    res.status(500).json({ message: "Something is wrong. Try again" });
+  }
 });
 
-router.post("/login", async(req, res)=>{
-    [
-        check('email','Pleas writte correct email!').normalizeEmail().isEmail(),
-        check('password','Not correct password!')
-    ]
-  async (req,res) => {
-      try {
-          const errors = validaionResult(req)
-
-          if(!errors.isEmpty()){
-              return res.status(400).json({
-                  errors:errors.array(),
-                  message:'Ivalid data login'
-              })
-          }
-
-          const {email,password} = req.body
-          const user = await User.findOne({ email })
-
-          if(!user){
-              return res.status(400).json({message:'Not fiend user :('})
-          }
-          const isMatchPassword = await bcrypt.compare(password,user.password)
-
-          if(!isMatchPassword){
-              return res.status(400).json({message:'Not fiend user :('})
-          }
-          const jwtSecret = 'igor-taras'
-          const token = jwt.sign(
-              { userId:user.id },
-              //Secreat Key, need create config default.json
-              jwtSecret,
-              {expiresIn: '1h'}
-          )
-          res.json({token,userId:user.id})
-
-
-      } catch (e) {
-          res.status(500).json({message:"Something is wrong. Try again"});
-      }
-      }
+// api/auth/login
+router.post("/login", async (req, res) => {
+  //   try {
+  //     //Need fixed
+  //     // const errors = validaionResult(req)
+  //     //
+  //     // if(!errors.isEmpty()){
+  //     //     return res.status(400).json({
+  //     //         errors:errors.array(),
+  //     //         message:'Ivalid data login'
+  //     //     })
+  //     // }
+  //     const { email, password } = req.body;
+  //     const user = await User.findOne({ email });
+  //     if (!user) {
+  //       return res.status(400).json({ message: "Not fiend user :(" });
+  //     }
+  //     const isMatchPassword = await bcrypt.compare(password, user.password);
+  //     if (!isMatchPassword) {
+  //       return res.status(400).json({ message: "Not fiend user :(" });
+  //     }
+  //     const jwtSecret = "igor-taras";
+  //     const token = jwt.sign(
+  //       { userId: user.id },
+  //       //Secreat Key, need create config default.json
+  //       jwtSecret,
+  //       { expiresIn: "1h" }
+  //     );
+  //     res.json({ token, userId: user.id });
+  //   } catch (e) {
+  //     res.status(500).json({ message: "Something is wrong. Try again" });
+  //   }
 });
 
 module.exports = router;
