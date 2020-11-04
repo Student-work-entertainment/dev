fdf28bf0138b67b6eec158ba19ed68ff1879b6c1
Taras <lytvynskyi.t@gmail.com>
2020-11-05 01:13:11 +0200

    Добавил логин в бэк и фронт, все ОК

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -7,6 +7,7 @@ const User = require("../models/User");
 const bcrypt = require("bcryptjs");
 
 const jwt = require("jsonwebtoken");
+const { check } = require("express-validator");
 
 // api/auth/register
 router.post("/register", async (req, res) => {
@@ -47,37 +48,44 @@ router.post("/register", async (req, res) => {
 });
 
 // api/auth/login
-router.post("/login", async (req, res) => {
-  //   try {
-  //     //Need fixed
-  //     // const errors = validaionResult(req)
-  //     //
-  //     // if(!errors.isEmpty()){
-  //     //     return res.status(400).json({
-  //     //         errors:errors.array(),
-  //     //         message:'Ivalid data login'
-  //     //     })
-  //     // }
-  //     const { email, password } = req.body;
-  //     const user = await User.findOne({ email });
-  //     if (!user) {
-  //       return res.status(400).json({ message: "Not fiend user :(" });
-  //     }
-  //     const isMatchPassword = await bcrypt.compare(password, user.password);
-  //     if (!isMatchPassword) {
-  //       return res.status(400).json({ message: "Not fiend user :(" });
-  //     }
-  //     const jwtSecret = "igor-taras";
-  //     const token = jwt.sign(
-  //       { userId: user.id },
-  //       //Secreat Key, need create config default.json
-  //       jwtSecret,
-  //       { expiresIn: "1h" }
-  //     );
-  //     res.json({ token, userId: user.id });
-  //   } catch (e) {
-  //     res.status(500).json({ message: "Something is wrong. Try again" });
-  //   }
-});
+router.post(
+  "/login",
+  [
+    check("email", "Invalid data :(").normalizeEmail().isEmail(),
+    check("password", "Invalid data :(").exists(),
+  ],
+  async (req, res) => {
+    try {
+      //Need fixed
+      // const errors = validaionResult(req)
+      //
+      // if(!errors.isEmpty()){
+      //     return res.status(400).json({
+      //         errors:errors.array(),
+      //         message:'Ivalid data login'
+      //     })
+      // }
+      const { email, password } = req.body;
+      const user = await User.findOne({ email });
+      if (!user) {
+        return res.status(400).json({ message: "Not fiend user :(" });
+      }
+      const isMatchPassword = await bcrypt.compare(password, user.password);
+      if (!isMatchPassword) {
+        return res.status(400).json({ message: "Not fiend user :(" });
+      }
+      const jwtSecret = "igor-taras";
+      const token = jwt.sign(
+        { userId: user.id },
+        //Secreat Key, need create config default.json
+        jwtSecret,
+        { expiresIn: "1h" }
+      );
+      res.json({ token, userId: user.id });
+    } catch (e) {
+      res.status(500).json({ message: "Something is wrong. Try again" });
+    }
+  }
+);
 
 module.exports = router;
